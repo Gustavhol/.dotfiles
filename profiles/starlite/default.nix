06af73ba552133913a8ab02a83880e727617e2bf
Gustav <gustav@holmstens.com>
2024-10-12 20:42:09 +0200

    add arc-browser to starlite

--- profiles/starlite/default.nix
+++ profiles/starlite/default.nix
@@ -100,6 +100,7 @@
     gnomeExtensions.screen-rotate # see https://www.reddit.com/r/starlabs_computers/comments/1dyebde/starlite_mkv_autorotate_solution/
     krita
     flashrom
+    arc-browser
   ];
 
 
