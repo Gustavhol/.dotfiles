1ba54681a0a81492781f0e5e05eb9bb0cb1795d8
gustavhol <gustav@holmsten.xyz>
2024-11-02 08:48:44 +0100

    plasma and vikunja to separate files

---
+++ services/vikunja/default.nix
@@ -0,0 +1,65 @@
+{config, pkgs, lib, ...}:
+
+
+
+{
+  networking = {
+    firewall = { 
+      enable = true;
+      allowedTCPPorts = [
+        3456
+      ];
+    };   
+  };
+ services = {
+  vikunja = {
+    enable = true;
+    port = 3456;
+    database = {
+      type = "postgres";
+      user = "vikunja";
+      database = "vikunja";
+      host= "/run/postgresql";
+    };
+    frontendHostname = "vikunja.holmstens.com";
+    frontendScheme = "https";
+    settings = {
+      service = {
+        motd = "Testmeddelande";
+        timezone = "Europe/Stockholm"
+      };
+      auth = {
+        local = {
+          enabled = true;
+        };
+      };
+    };
+  };
+  postgresql = {
+    ensureDatabases = [ "vikunja" ];
+    ensureUsers = [
+        {
+            name = "vikunja";
+            ensureDBOwnership = true;
+        }
+    ];
+  };
+ };
+
+  systemd.services.vikunja-api = {
+    serviceConfig = {
+      DynamicUser = lib.mkForce false;
+      User = "vikunja";
+      Group = "vikunja";
+    };
+  };
+
+  users.users.vikunja = {
+    description = "Vikunja Service";
+    createHome = false;
+    group = "vikunja";
+    isSystemUser = true;
+  };
+
+  users.groups.vikunja = { };
+}
