802e34b7c4cd4ca85f8f62fd0a8c20451b51df46
Gustav <gustav@holmstens.com>
2025-03-24 14:20:31 +0100

    add gsconnect to starlite

--- profiles/starlite/default.nix
+++ profiles/starlite/default.nix
@@ -13,9 +13,6 @@
       ../common/pipewire.nix
     ];
 
-
-  # networking.hostName = "starlite"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networking = {
     hostName = "starlite"; # Define your hostname.
     firewall = { 
@@ -29,20 +26,9 @@
     };   
   };
 
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-    # Enable automatic login for the user.
-  # services.xserver.displayManager.autoLogin.enable = true;
-  # services.xserver.displayManager.autoLogin.user = "gustav";
-
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
-
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
 
   services = {
       libinput.enable = true;
@@ -81,39 +67,18 @@
     seahorse
   ]);
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     nixd
     gnomeExtensions.appindicator # tray icon
     gnomeExtensions.screen-rotate # see https://www.reddit.com/r/starlabs_computers/comments/1dyebde/starlite_mkv_autorotate_solution/
     krita
     flashrom
-    # arc-browser
   ];
 
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+  };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Do not change!!!
 }
