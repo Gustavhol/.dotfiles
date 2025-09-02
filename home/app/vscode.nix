e379a1d29bb6a3def2690c312f5d7cfed941d2f1
gustavhol <gustav@holmsten.xyz>
2025-09-02 19:30:35 +0200

    break out vscode home-manager settings to separate module

---
+++ home/app/vscode.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.vscode];
+
+  programs.vscode = {
+    enable = true;
+
+    extensions = with pkgs.vscode-extensions; [
+      bbenoist.nix
+      jnoortheen.nix-ide
+      william-voyek.vscode-nginx
+      teclado.vscode-nginx-formatter
+      tailscale.vscode-tailscale
+      ms-python.python
+      ms-vscode.cpptools
+      eamodio.gitlens
+      ms-azuretools.vscode-docker
+      esbenp.prettier-vscode
+      dbaeumer.vscode-eslint
+      ms-vscode.vscode-typescript-next
+    ];
+
+    profiles.gustav.userSettings = {
+      "nix" = {
+        "enableLanguageServer" = true;
+        "serverPath" = "nixd";
+        "formatterPath" = "alejandra";
+        "serverSettings" = {
+          "nixd" = {
+            "formatting" = {
+              "command" = ["alejandra"];
+            };
+            "options" = {
+              "nixos" = {
+                "expr" = "(builtins.getFlake \"/home/gustav/.dotfiles\").nixosConfigurations.golmsten.options";
+              };
+              "home_manager" = {
+                "expr" = "(builtins.getFlake \"/home/gustav/.dotfiles\").homeConfigurations.gustav.options";
+              };
+            };
+          };
+        };
+      };
+
+      "git.autofetch" = true;
+      "git.confirmSync" = false;
+      "[nix]" = {
+        "editor.formatOnSave" = true;
+      };
+      "editor.fontSize" = 13;
+    };
+  };
+}
--- home/gustav.nix
+++ home/gustav.nix
@@ -1,13 +1,20 @@
-{ config, pkgs, unstablePkgs, systemSettings, userSettings, inputs, system, ... }:
-
 {
+  config,
+  pkgs,
+  unstablePkgs,
+  systemSettings,
+  userSettings,
+  inputs,
+  system,
+  ...
+}: {
   # Enable the Home-Manager service itself
   programs.home-manager.enable = true;
 
   # Your user & directory
-  home.username      = userSettings.username;
+  home.username = userSettings.username;
   home.homeDirectory = "/home/${userSettings.username}";
-  home.stateVersion  = "23.11";  # adjust when you bump major versions
+  home.stateVersion = "23.11"; # adjust when you bump major versions
 
   # Import extra Home-Manager modules
   imports = [
@@ -17,17 +24,33 @@
     ./app/git.nix
     ./app/kitty.nix
     ./app/nixvim.nix
+    ./app/vscode.nix
     ./app/zsh.nix
     # inputs.firefox-addons.homeManagerModules.default
     # inputs.nix-minecraft.homeManagerModules.default
   ];
 
   # Packages: stable pkgs + a few unstable apps
-  home.packages = (with pkgs; [
-    lunarvim unicode-emoji starship eza flameshot transmission_4 vscode vlc
-  ]) ++ (with unstablePkgs; [
-    orca-slicer beeper google-chrome obsidian rpi-imager spotify vivaldi
-  ]);
+  home.packages =
+    (with pkgs; [
+      lunarvim
+      unicode-emoji
+      starship
+      eza
+      flameshot
+      transmission_4
+      # vscode
+      vlc
+    ])
+    ++ (with unstablePkgs; [
+      orca-slicer
+      beeper
+      google-chrome
+      obsidian
+      rpi-imager
+      spotify
+      vivaldi
+    ]);
 
   # Dotfiles, session vars, etc.
   # home.file = {
@@ -36,15 +59,15 @@
 
   home.sessionVariables = {
     EDITOR = userSettings.editor;
-    TERM   = userSettings.term;
+    TERM = userSettings.term;
   };
 
   # Shells, prompts, media player
-  programs.zsh.enable       = true;
-  programs.starship.enable  = true;
+  programs.zsh.enable = true;
+  programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
-  programs.bash.enable      = true;
-  programs.mpv.enable       = true;
+  programs.bash.enable = true;
+  programs.mpv.enable = true;
 
   # Firefox + extensions
   programs.firefox = {
@@ -52,7 +75,12 @@
     profiles = {
       "${userSettings.username}" = {
         extensions.packages = with inputs.firefox-addons.packages.${system}; [
-          bitwarden ublock-origin sponsorblock decentraleyes privacy-badger clearurls
+          bitwarden
+          ublock-origin
+          sponsorblock
+          decentraleyes
+          privacy-badger
+          clearurls
         ];
       };
     };
